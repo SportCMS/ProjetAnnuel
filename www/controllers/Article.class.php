dcb6264e890ba72b9af412b46213aed486dfc6e7
Choetsu <samy.amh94@gmail.com>
2022-05-24 15:59:08 +0200

    Mise à jour fonctionnalité article
    Ajout détail article + affichage des article

--- www/controllers/Article.class.php
+++ www/controllers/Article.class.php
@@ -61,5 +61,37 @@ class Article extends Sql{
 		}
 	}
 
-	
+	public function detailsArticle() {
+		$article = new ArticleModel();
+		$view = new View("detailsarticle");
+
+		$article_id = $_GET['article_id'];
+		
+		$article_details = $article->getOneBy(['id' => $article_id]);
+		$object = $article_details[0];
+
+		$id = $object->id;
+		$title = $object->title;
+		$content = $object->content;
+		$category_id = $object->category_id;
+	}
+
+	public function allArticle() {
+		$view = new View("articles");
+		$article = new ArticleModel();
+
+		$all_article = $article->getAll();
+		
+
+		$view->assign("all_article", $all_article);
+
+	}
+
+	public function updateArticle() {
+
+	}
+
+	public function deleteArticle() {
+
+	}
 }
