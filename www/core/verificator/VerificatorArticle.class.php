62d33c09caa4410fa86fdd1e808adcf37221d544
mchemlal <michel.chemlal@gmail.com>
2022-05-24 15:52:10 +0200

    Mise en place d'un verificator article
    creation d'un dossier verificator, creation d'un modèle verificatore article pour la validation des données

---
+++ www/core/verificator/VerificatorArticle.class.php
@@ -0,0 +1,30 @@
+<?php
+    namespace App\Core\verificator;
+
+    
+    class VerificatorArticle
+    {
+		public static function validate($config, array $data): array
+		{
+			if( count($config["inputs"]) != count($_POST) && count($config["inputs"]) != count($_GET) ){
+                	die("Tentative de hack");
+            }
+		
+			$errors = [];
+		
+			if (!$data['category_id'] || !intval($data['category_id']))
+			$errors['categories'] =  'Catégorie non renseignée';
+		
+			if (!$data['title'])
+			$errors['title'] =  'titre non renseignée';
+		
+			if (strlen($data['title']) > 100)
+			$errors['title'] =  'La taille max est de 100 caractères';
+		
+			if (!$data['content'])
+			$errors['content'] =  'Veuillez renseigner un contenu';
+		
+			return $errors;
+		}
+    }
+
