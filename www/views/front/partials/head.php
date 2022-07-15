08337dbc85cb968c40ca9164c5f716865d7aaaff
simonBtssio <simon.farnault.sio@gmail.com>
2022-07-15 12:48:26 +0200

    meta data

--- www/views/front/partials/head.php
+++ www/views/front/partials/head.php
@@ -1,4 +1,5 @@
 <head>
+    <meta name="language" content="fr-FR">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,4 +8,10 @@
     <link rel="stylesheet" href="public/dist/form.css">
     <script src="public/src/js/vendor/jquery-3.6.0.min.js"></script>
     <script src="public/src/js/main.js"></script>
+    <meta name="title" content="SportCMS">
+    <meta name="description" content="SportCMS vous permet de créer votre propre blog dédié au sport">
+    <meta name="keywords" content="Sport, CMS, Club, salle, compétition, actualités, football, tennis">
+    <meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="2 days">
+    <meta name="author" content="Morade, Antoine, Ayman, Muthulan, Samy, Simon">
 </head>
